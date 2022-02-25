8ece2851d3e7bc43004ba5b338d9d4db6ecdf05e
tjcsilicon <92831209+tjcsilicon@users.noreply.github.com>
2022-02-24 18:46:54 -0800

    VS Project created. Added node.h and GMAP

---
+++ node.h
@@ -0,0 +1,16 @@
+
+class person
+{
+	int u;
+	int v;
+	double w;
+	int t;
+	//add edges array?
+};
+
+int isInfected(person p1, person p2)
+{
+	//TODO::
+	//function that takes two person objects and returns true if the sum of all connected edges to infected people
+	//is greater than the tolerance of the person
+}
